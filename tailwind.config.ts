e82b3d8159524416408bb648ee1ee200b8832b31
humblegitter <crystalball1901@gmail.com>
2024-12-17 13:28:37 -0500

    Fixed error where Fonts werent loading across different browsers

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        daydream: ['var(--font-daydream)'],
+      },
     },
   },
   plugins: [],
